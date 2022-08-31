ef71876e62e0d49d8f8af867d3524136ad4298bb
raddari <harveywyatt@hotmail.com>
2022-09-01 00:39:50 +0930

    chore: update user settings

--- lua/config/settings/options.lua
+++ lua/config/settings/options.lua
@@ -9,6 +9,7 @@ opt.completeopt:append({
   'noinsert',
   'noselect',
 })
+opt.conceallevel = 2
 opt.confirm = true
 opt.cursorline = true
 opt.expandtab = true
@@ -22,23 +23,36 @@ opt.fillchars:append({
   verthoriz = '╋',
 })
 opt.hidden = true
+opt.hlsearch = false
 opt.ignorecase = true
 opt.joinspaces = true
 opt.laststatus = 3
 opt.list = true
+opt.matchpairs:append({ '<:>' })
 opt.mouse = ''
+opt.nrformats:append({ 'alpha' })
 opt.number = true
+opt.path:remove({ '/usr/include' })
+opt.path:append({ '**' })
+opt.pumblend = 10
+opt.pumheight = 10
 opt.relativenumber = true
 opt.scrolloff = 9
 opt.shiftround = true
 opt.shiftwidth = 2
+opt.showmode = false
 opt.sidescrolloff = 9
 opt.signcolumn = 'yes:2'
 opt.smartcase = true
+opt.softtabstop = 2
 opt.splitbelow = true
 opt.splitright = true
 opt.swapfile = true
 opt.tabstop = 2
 opt.termguicolors = true
-opt.updatetime = 500
+opt.undofile = true
+opt.undolevels = 150
+opt.updatetime = 400
+opt.wildignore:append({ '*/.git/*' })
+opt.wildmenu = true
 opt.wildmode = { 'longest:list', 'full' }
