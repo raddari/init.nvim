e8e8faf7dc65c509730fabdf7d09e44da4986d74
raddari <harveywyatt@hotmail.com>
2022-08-31 22:00:02 +0930

    feat: gitsigns config

--- lua/config/plugins/gitsigns/setup.lua
+++ lua/config/plugins/gitsigns/setup.lua
@@ -1,1 +1,8 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+  current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text_pos = 'right_align',
+    delay = 500,
+  },
+  preview_config = { border = 'none' },
+})
