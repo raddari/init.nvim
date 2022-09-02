55fde3d1cabf2c8e136526b9fb1476dcc5b87a58
raddari <harveywyatt@hotmail.com>
2022-09-02 19:15:57 +0930

    chore(lsp_signature): remove border from popup

--- lua/config/plugins/lsp_signature/setup.lua
+++ lua/config/plugins/lsp_signature/setup.lua
@@ -2,7 +2,7 @@ require('lsp_signature').setup({
   bind = true,
   fix_pos = false, -- change to true if the disappearing act annoys me
   handler_opts = {
-    border = nil,
+    border = 'none',
   },
   hint_enable = false,
   toggle_key = '<M-x>',
