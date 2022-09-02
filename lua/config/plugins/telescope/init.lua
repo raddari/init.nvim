44ef60485c93914cbbceac33e39d6187ff7660a4
raddari <harveywyatt@hotmail.com>
2022-09-02 20:08:04 +0930

    feat(telescope): lazy load to improve startup time

--- lua/config/plugins/telescope/init.lua
+++ lua/config/plugins/telescope/init.lua
@@ -1,20 +1,53 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
-  config = function()
-    require('config.plugins.telescope.setup')
-    require('config.plugins.telescope.keymap')
-  end,
-  event = 'BufWinEnter',
-  requires = {
-    'nvim-lua/popup.nvim',
-    'nvim-lua/plenary.nvim',
-    -- extensions
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    config = function()
+      require('config.plugins.telescope.setup')
+      require('config.plugins.telescope.keymap')
+    end,
+    module = 'telescope',
+    requires = {
+      { 'nvim-lua/popup.nvim' },
+      { 'nvim-lua/plenary.nvim' },
+    },
+  },
+  -- extensions
+  { 'nvim-telescope/telescope-symbols.nvim', after = 'telescope.nvim' },
+  {
     'benfowler/telescope-luasnip.nvim',
+    config = function()
+      require('telescope').load_extension('luasnip')
+    end,
+    after = 'telescope.nvim',
+  },
+  {
     'LinArcX/telescope-env.nvim',
+    config = function()
+      require('telescope').load_extension('env')
+    end,
+    after = 'telescope.nvim',
+  },
+  {
     'nvim-telescope/telescope-file-browser.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    config = function()
+      require('telescope').load_extension('file_browser')
+    end,
+    after = 'telescope.nvim',
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    config = function()
+      require('telescope').load_extension('fzf')
+    end,
+    after = 'telescope.nvim',
+    run = 'make',
+  },
+  {
     'nvim-telescope/telescope-packer.nvim',
-    'nvim-telescope/telescope-symbols.nvim',
+    config = function()
+      require('telescope').load_extension('packer')
+    end,
+    after = 'telescope.nvim',
   },
 }
--- lua/config/plugins/telescope/setup.lua
+++ lua/config/plugins/telescope/setup.lua
@@ -1,10 +1,1 @@
-local telescope = require('telescope')
-
-telescope.setup({
-})
-
-telescope.load_extension('env')
-telescope.load_extension('fzf')
-telescope.load_extension('file_browser')
-telescope.load_extension('luasnip')
-telescope.load_extension('packer')
+require('telescope').setup({})
