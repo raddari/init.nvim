c68cedc0270c50af1ca41773eb79879e9689f488
raddari <harveywyatt@hotmail.com>
2022-09-03 23:18:01 +0930

    feat(zen-mode): add setup configuration

--- lua/config/plugins/zen-mode.lua
+++ lua/config/plugins/zen-mode.lua
@@ -1,1 +1,28 @@
-require('zen-mode').setup()
+require('zen-mode').setup({
+  window = {
+    backdrop = 0.95,
+  },
+  plugins = {
+    gitsigns = { enabled = true },
+    tmux = { enabled = true },
+  },
+  on_open = function(_)
+    local gs = require('gitsigns')
+    local config = require('gitsigns.config').config
+
+    local state = require('zen-mode.view').state
+    state.current_line_blame = config.current_line_blame
+    config.current_line_blame = false
+
+    gs.refresh()
+  end,
+  on_close = function()
+    local gs = require('gitsigns')
+    local config = require('gitsigns.config').config
+
+    local state = require('zen-mode.view').state
+    config.current_line_blame = state.current_line_blame
+
+    gs.refresh()
+  end,
+})
