53a8cfd4961a548c5926f90c891cc25f8bf661c3
raddari <harveywyatt@hotmail.com>
2022-09-14 01:45:23 +0930

    feat: add gf support for lua require

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,1 +1,11 @@
 vim.b.format_on_save = true
+
+-- https://github.com/sam4llis/nvim-lua-gf/blob/ca712497b2bab6351518917be219e9bfd8d63e4f/after/ftplugin/lua.lua
+local opt = vim.opt_local
+opt.include = [[\v<((do|load)file|require)[^''"]*[''"]\zs[^''"]+]]
+opt.includeexpr = "substitute(v:fname,'\\.','/','g')"
+opt.suffixesadd:prepend('.lua')
+
+for _, path in pairs(vim.api.nvim_list_runtime_paths()) do
+  opt.path:append(path .. '/lua')
+end
