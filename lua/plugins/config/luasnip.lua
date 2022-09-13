17b5e7df28e8811038c1f96fa890bf1e5d2670b2
raddari <harveywyatt@hotmail.com>
2022-09-14 01:47:54 +0930

    refactor(luasnip): cleanup config

--- lua/plugins/config/luasnip.lua
+++ lua/plugins/config/luasnip.lua
@@ -1,9 +1,6 @@
-local ls = require('luasnip')
-
-ls.config.set_config({
+require('luasnip').config.set_config({
   history = true,
   updateevents = 'TextChanged,TextChangedI',
-  enable_autosnippets = true,
 })
 
 require('luasnip.loaders.from_vscode').lazy_load()
