0e1c38d76aa408825bc71de4d408ee5ce20d3ec5
raddari <harveywyatt@hotmail.com>
2022-09-14 01:47:21 +0930

    config(mason-lspconfig): remove ensure_installed option

--- lua/plugins/config/mason-lspconfig.lua
+++ lua/plugins/config/mason-lspconfig.lua
@@ -1,14 +1,3 @@
 require('mason-lspconfig').setup({
-  ensured_installed = {
-    'clangd',
-    'cmake',
-    'jsonls',
-    'marksman',
-    'pyright',
-    'rust_analyzer@nightly',
-    'sumneko_lua',
-    'taplo',
-    'yamlls',
-    'zls',
-  },
+  automatic_installation = true,
 })
