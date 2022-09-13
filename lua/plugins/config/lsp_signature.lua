c74d36ce17c9f7a1fa085d6198c326919a1acc3e
raddari <harveywyatt@hotmail.com>
2022-09-14 01:48:43 +0930

    config(lsp_signature): change border

--- lua/plugins/config/lsp_signature.lua
+++ lua/plugins/config/lsp_signature.lua
@@ -1,8 +1,10 @@
+local icons = require('theme.icons')
+
 require('lsp_signature').setup({
   bind = true,
   fix_pos = false, -- change to true if the disappearing act annoys me
   handler_opts = {
-    border = 'none',
+    border = icons.border,
   },
   hint_enable = false,
   toggle_key = '<C-k>',
