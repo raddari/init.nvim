0cffe0ba342ab9fd4e0df607246004ff168e8533
raddari <harveywyatt@hotmail.com>
2022-09-14 16:55:29 +0930

    fix(cmp): error during comment context

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -6,14 +6,6 @@ cmp.setup({
   completion = {
     autocomplete = false,
   },
-  enabled = function()
-    local context = require('cmp.config.context')
-    if vim.api.nvim_get_mode().mode == 'c' then
-      return true
-    else
-      return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
-    end
-  end,
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
