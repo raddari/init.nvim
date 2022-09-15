8555c37cc0f0c34c5df98cf0b4c92be0aebf0b87
raddari <harveywyatt@hotmail.com>
2022-09-15 15:01:08 +0930

    config(sumneko_lua): remove vim specific setup
    lua-dev.nvim handles this for us

--- lua/lsp/servers/sumneko_lua.lua
+++ lua/lsp/servers/sumneko_lua.lua
@@ -1,13 +1,6 @@
 local config = { settings = {} }
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
 config.settings.Lua = {
-  diagnostics = {
-    globals = { 'vim' },
-  },
   format = {
     enable = false,
   },
@@ -16,14 +9,10 @@ config.settings.Lua = {
     setType = true,
   },
   runtime = {
-    path = runtime_path,
     version = 'LuaJIT',
   },
   telemetery = {
     enable = false,
-  },
-  workspace = {
-    library = vim.api.nvim_get_runtime_file('lua', true),
   },
   completion = {
     callSnippet = 'Replace',
