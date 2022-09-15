beb5bd409deaf1a51277f1e82236f4e4492f64c3
raddari <harveywyatt@hotmail.com>
2022-09-15 15:01:53 +0930

    config(treesitter): remove ensure_installed option

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -1,45 +1,15 @@
 require('nvim-treesitter.configs').setup({
   auto_install = true,
-  ensure_installed = {
-    'bash',
-    'c',
-    'cmake',
-    'cpp',
-    'css',
-    'fish',
-    'html',
-    'java',
-    'javascript',
-    'jsdoc',
-    'json',
-    'lua',
-    'make',
-    'markdown',
-    'markdown_inline',
-    'python',
-    'regex',
-    'rust',
-    'scss',
-    'toml',
-    'tsx',
-    'typescript',
-    'vim',
-    'yaml',
-    'zig',
-  },
   highlight = {
     enable = true,
-    use_languagetree = true,
+    custom_captures = {},
   },
   indent = { enable = false },
+  incremental_selection = { enable = true },
   autotag = { enable = true },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
-  },
-  refactor = {
-    highlight_definitions = { enable = true },
-    highlight_current_scope = { enable = false },
   },
   textobjects = {
     select = {
@@ -57,6 +27,19 @@ require('nvim-treesitter.configs').setup({
         ['@class.outer'] = '<c-v>', -- blockwise
       },
       include_surrounding_whitespace = true,
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        [']vp'] = '@parameter.inner',
+        [']vm'] = '@function.outer',
+        [']vc'] = '@class.outer',
+      },
+      swap_previous = {
+        ['[vp'] = '@parameter.inner',
+        ['[vm'] = '@function.outer',
+        ['[vc'] = '@class.outer',
+      },
     },
     move = {
       enable = true,
