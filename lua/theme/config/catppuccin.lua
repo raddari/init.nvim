a7e1a369403d88e10d94245ea19e29b07775067e
raddari <harveywyatt@hotmail.com>
2022-10-23 19:47:15 +1030

    refactor(theme.catppuccin): disable italics

--- lua/theme/config/catppuccin.lua
+++ lua/theme/config/catppuccin.lua
@@ -3,19 +3,39 @@ local M = {}
 M.config = function()
   vim.g.catppuccin_flavour = 'mocha'
 
+  local colors = require('catppuccin.palettes').get_palette()
+
   require('catppuccin').setup({
     dim_inactive = { enabled = false },
-    term_colors = true,
     styles = {
       comments = {},
       conditionals = {},
     },
     integrations = {
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
       dap = {
         enabled = true,
         enabled_gui = true,
       },
-      which_key = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = {},
+          hints = {},
+          warnings = {},
+          information = {},
+        },
+      },
+    },
+    custom_highlights = {
+      ['@namespace'] = { fg = colors.yellow, style = {} },
+      ['@parameter'] = { fg = colors.maroon, style = {} },
+      ['@tag.attribute'] = { fg = colors.teal, style = {} },
+      ['@text.literal'] = { fg = colors.teal, style = {} },
+      ['@text.uri'] = { fg = colors.rosewater, style = { 'underline' } },
+      ['@type.builtin'] = { fg = colors.yellow, style = {} },
     },
   })
 end
