1888d7c57096accb48648e6793ad20472c190de6
raddari <harveywyatt@hotmail.com>
2022-11-17 22:27:32 +1030

    fix(sumneko_lua): add vim global

--- lua/lsp/servers/sumneko_lua.lua
+++ lua/lsp/servers/sumneko_lua.lua
@@ -1,6 +1,11 @@
 local config = { settings = {} }
 
+config.before_init = require('neodev.lsp').before_init
+
 config.settings.Lua = {
+  diagnostics = {
+    globals = { 'vim' },
+  },
   format = {
     enable = false,
   },
