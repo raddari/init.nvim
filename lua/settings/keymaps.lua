943e04233e39d287b2ed65283f169ce8925aa5ea
raddari <harveywyatt@hotmail.com>
2022-11-25 16:26:35 +1030

    feat: add delete buffer keymap

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -11,3 +11,4 @@ map('n', '<Leader>\\', '<Cmd>vsplit<CR>', { desc = 'Vertical split' })
 map('n', '<Leader>-', '<Cmd>split<CR>', { desc = 'Horizontal split' })
 -- util
 map('n', '<Leader>lo', '<Cmd>source %<CR>', { desc = 'Source current file' })
+map('n', '<Leader>bd', '<Cmd>bd<CR>', { desc = 'Remove current buffer' })
