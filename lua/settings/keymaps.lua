821032b13bc4f8ee88353442e8c2dd5d9855171a
raddari <harveywyatt@hotmail.com>
2022-12-07 00:22:30 +1030

    config(keymaps): undisable arrow keys + backspace

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -1,11 +1,4 @@
 local map = vim.keymap.set
--- disable arrow keys
-map({ 'n', 'i', 'v' }, '<Up>', '<Nop>')
-map({ 'n', 'i', 'v' }, '<Down>', '<Nop>')
-map({ 'n', 'i', 'v' }, '<Left>', '<Nop>')
-map({ 'n', 'i', 'v' }, '<Right>', '<Nop>')
--- and backspace
-map('i', '<BS>', '<Nop>')
 -- splits
 map('n', '<Leader>\\', '<Cmd>vsplit<CR>', { desc = 'Vertical split' })
 map('n', '<Leader>-', '<Cmd>split<CR>', { desc = 'Horizontal split' })
