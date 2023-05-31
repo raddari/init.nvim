76f84cd5f97f85d94fdb7ff3e33642ada556133b
raddari <harveywyatt@hotmail.com>
2023-06-01 00:08:53 +0930

    cleanup: remove unused lua_ls settings

--- lua/lsp/servers/lua_ls.lua
+++ lua/lsp/servers/lua_ls.lua
@@ -1,29 +1,13 @@
-local config = { settings = {} }
+local config = {}
 
-config.before_init = require('neodev.lsp').before_init
-
-config.settings.Lua = {
-  diagnostics = {
-    globals = { 'vim' },
-  },
-  format = {
-    enable = false,
-  },
-  hint = {
-    enable = true,
-    setType = true,
-  },
-  runtime = {
-    version = 'LuaJIT',
-  },
-  telemetery = {
-    enable = false,
-  },
-  completion = {
-    callSnippet = 'Replace',
-  },
-  workspace = {
-    checkThirdParty = false,
+config.settings = {
+  Lua = {
+    completion = {
+      callSnippet = 'Replace',
+    },
+    format = {
+      enable = false,
+    },
   },
 }
 
