0230eed009f46f1a97d2ec336b31a18b7a8e8dd2
raddari <harveywyatt@hotmail.com>
2023-06-01 00:09:17 +0930

    fix: use null-ls for lua formatting

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,4 +1,5 @@
 vim.b.format_on_save = true
+vim.b.format_provider = 'null-ls'
 
 -- https://github.com/sam4llis/nvim-lua-gf/blob/ca712497b2bab6351518917be219e9bfd8d63e4f/after/ftplugin/lua.lua
 local opt = vim.opt_local
