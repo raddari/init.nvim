7b909c0f8a5195c10cf7df02db8da60400840c98
raddari <harveywyatt@hotmail.com>
2023-06-06 20:54:13 +0930

    config: add build keymaps

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -5,3 +5,5 @@ map('n', '<Leader>-', '<Cmd>split<CR>', { desc = 'Horizontal split' })
 -- util
 map('n', '<Leader>lo', '<Cmd>source %<CR>', { desc = 'Source current file' })
 map('n', '<Leader>bd', '<Cmd>bd<CR>', { desc = 'Remove current buffer' })
+map('n', '<Leader>m', '<Cmd>silent make!<CR><Cmd>cope<CR>', { desc = 'Build and open compiler output' })
+map('n', '<Leader>M', '<Cmd>make!<CR>', { desc = 'Build' })
