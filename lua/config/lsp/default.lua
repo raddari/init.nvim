c94aee0d960eeae6d2628faa8a9caf924e995aab
raddari <harveywyatt@hotmail.com>
2023-07-01 22:58:58 +0930

    refactor: allow lsp to lazy load

--- lua/config/lsp/default.lua
+++ lua/config/lsp/default.lua
@@ -1,10 +1,10 @@
-local lsp_attach = require('config.lsp.attach')
 local M = {}
 
 -- TODO(raddari): refactor this into cmp config
 M.capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 M.on_attach = function(client, bufnr)
+  local lsp_attach = require('config.lsp.attach')
   lsp_attach.keymaps(client, bufnr)
   lsp_attach.codelens(client, bufnr)
   lsp_attach.format_on_save(client, bufnr)
