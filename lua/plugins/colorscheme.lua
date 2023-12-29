58aff2bcc2a6e4ab29b0729628084e9ea70ece1a
raddari <raddari@tuta.io>
2023-12-29 11:50:19 +1030

    config(colorscheme): use oxocarbon

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,3 @@
 return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      no_italic = true,
-      integrations = {
-        mini = true,
-        nvimtree = false,
-      },
-    },
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-      },
-    },
-  },
+  { 'nyoom-engineering/oxocarbon.nvim' },
 }
